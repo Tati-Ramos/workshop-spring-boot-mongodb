4d319ee9569d9c2bd4fc9cada59517bfab072051
Tatiane Ramos <tatiane_sr@outlook.com>
2022-03-18 21:01:41 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/tatiramos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tatiramos/workshopmongo/config/Instantiation.java
@@ -42,8 +42,6 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-	}
-	
-	
+	}	
 
 }
