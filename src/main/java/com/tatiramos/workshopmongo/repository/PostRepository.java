842fed1368856bc9ca8079504fccef23da5c42e8
Tatiane Ramos <tatiane_sr@outlook.com>
2022-03-18 22:12:04 -0300

    Consulta simples com @Query

--- src/main/java/com/tatiramos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/tatiramos/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.tatiramos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tatiramos.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")	
+	List<Post> searchTitle(String tex);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
